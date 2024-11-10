e515b3c1bbc5401d8057ab241781b62030824cb9
GLAUSER Vincent <vincent.glauser@unifr.ch>
2024-11-10 16:29:26 +0100

    Resolve merge conflict

--- main_multiple_parameter_2_std_matrix.jl
+++ main_multiple_parameter_2_std_matrix.jl
@@ -83,8 +83,12 @@ function scatter_plot_for_mulitple_gml(;
 
                                 total_path=save_path*filename
 
-                                if(total_path*".gml" in path_array)
+                                if(total_path*".gml" in path_array && 
+                                    #filename!="m_BTMC_N=216_T=0.17_Beta=0.0_GradT=0.1_StepsPerT=0.01_Theta_GS=110.0_Trial=1" &&
+                                    filename!="m_BTMC_q_t__N=216_T=0.1_Beta=0.0_GradT=0.1_StepsPerT=0.01_Theta_GS=110.0_Trial=1"
+                                    )
                                     #println("scatter done")
+                                    println(filename)
 
                                     spatial_network=NG.load_spatial_network_from_gml(total_path*".gml")
 
@@ -115,7 +119,7 @@ function scatter_plot_for_mulitple_gml(;
                                             maximal_temperature,
                                             bond_bending_const,
                                             temperature_gradient,
-                                            #nr_monte_carlo_steps_per_temperature,
+                                            nr_monte_carlo_steps_per_temperature,
                                             theta_ground_state,
                                             #i,
                                             bond_length_std,
@@ -146,7 +150,90 @@ function scatter_plot_for_mulitple_gml(;
 
     df=DataFrames.DataFrame(matrix,:auto)
 
-    DataFrames.rename!(df, [:x1, :x2, :x3, :x4, :x5, :x6, :x7] .=>  [:MaxT, :Beta, :GradT,  :Theta, :BondLenghtStd, :BondAngleStd, :AcceptedMoves])
+    DataFrames.rename!(df, [:x1, :x2, :x3, :x4, :x5, :x6, :x7, :x8] .=>  [:MaxT, :Beta, :GradT, :MCsteps, :Theta, :BondLenghtStd, :BondAngleStd, :AcceptedMoves])
+
+
+
+    println(df)
+
+
+    df.BondLenghtStdDiff.=0.0
+    df.BondAngleStdDiff.=0.0
+
+    for (i, row) in enumerate( eachrow( df ) ) 
+        println(i)
+        
+        if i<size(df,1)
+            if ((df[i,"MaxT"]===df[i+1,"MaxT"]) &&
+                (df[i,"Beta"]===df[i+1,"Beta"]) && 
+                (df[i,"GradT"]===df[i+1,"GradT"]) && 
+                (df[i,"MCsteps"]===df[i+1,"MCsteps"]) && 
+                (df[i,"Theta"]!=df[i+1,"Theta"]))
+
+                if df[i,"Theta"]===180.0 && df[i+1,"Theta"]===110.0
+
+                    df[i,"BondLenghtStdDiff"]=df[i,"BondLenghtStd"]-df[i+1,"BondLenghtStd"]
+                    df[i,"BondAngleStdDiff"]=df[i,"BondAngleStd"]-df[i+1,"BondAngleStd"]
+
+                elseif df[i+1,"Theta"]===180.0 && df[i,"Theta"]===110.0
+
+                    df[i,"BondLenghtStdDiff"]=df[i+1,"BondLenghtStd"]-df[i,"BondLenghtStd"]
+                    df[i,"BondAngleStdDiff"]=df[i+1,"BondAngleStd"]-df[i,"BondAngleStd"]
+                
+                else
+                    println("i+1: Theta not 180 and 110")
+                end
+            elseif (i>1 && (
+                (df[i,"MaxT"]===df[i-1,"MaxT"]) && 
+                (df[i,"Beta"]===df[i-1,"Beta"]) && 
+                (df[i,"GradT"]===df[i-1,"GradT"]) && 
+                (df[i,"MCsteps"]===df[i-1,"MCsteps"]) && 
+                (df[i,"Theta"]!=df[i-1,"Theta"])))
+
+                if df[i,"Theta"]===180.0 && df[i-1,"Theta"]===110.0
+
+                    df[i,"BondLenghtStdDiff"]=df[i,"BondLenghtStd"]-df[i-1,"BondLenghtStd"]
+                    df[i,"BondAngleStdDiff"]=df[i,"BondAngleStd"]-df[i-1,"BondAngleStd"]
+
+                elseif df[i-1,"Theta"]===180.0 && df[i,"Theta"]===110.0
+
+                    df[i,"BondLenghtStdDiff"]=df[i-1,"BondLenghtStd"]-df[i,"BondLenghtStd"]
+                    df[i,"BondAngleStdDiff"]=df[i-1,"BondAngleStd"]-df[i,"BondAngleStd"]
+                
+                else
+                    println("i-1: Theta not 180 and 110")
+                end
+            else
+                println("Beta, GradT, MCsteps not the same OR Theta the same")
+            end
+        else
+            if ((df[i,"MaxT"]===df[i-1,"MaxT"]) &&
+                (df[i,"Beta"]===df[i-1,"Beta"]) && 
+                (df[i,"GradT"]===df[i-1,"GradT"]) && 
+                (df[i,"MCsteps"]===df[i-1,"MCsteps"]) && 
+                (df[i,"Theta"]!=df[i-1,"Theta"]))
+
+                if df[i,"Theta"]===180.0 && df[i-1,"Theta"]===110.0
+
+                    df[i,"BondLenghtStdDiff"]=df[i,"BondLenghtStd"]-df[i-1,"BondLenghtStd"]
+                    df[i,"BondAngleStdDiff"]=df[i,"BondAngleStd"]-df[i-1,"BondAngleStd"]
+
+                elseif df[i-1,"Theta"]===180.0 && df[i,"Theta"]===110.0
+
+                    df[i,"BondLenghtStdDiff"]=df[i-1,"BondLenghtStd"]-df[i,"BondLenghtStd"]
+                    df[i,"BondAngleStdDiff"]=df[i-1,"BondAngleStd"]-df[i,"BondAngleStd"]
+                
+                else
+                    println("i-1: Theta not 180 and 110")
+                end
+            end
+        end
+    end
+
+
+    df.BondLenghtStdDiffLog10 = log10.(df.BondLenghtStdDiff .+ 1)
+    df.BondAngleStdDiffLog10 = log10.(df.BondAngleStdDiff .+ 1)
+
 
 
     df.Shape = @. ifelse(df.Theta == 110.0, :diamond, :rect); df
@@ -163,6 +250,7 @@ function scatter_plot_for_mulitple_gml(;
         #plot_title=plot_title,
         plot_title=LaTeXStrings.LaTeXString("Bond length std vs Bond angle std\n\$^\\textrm{"*
             "N="*"$(nr_vertices_array[1])" *", "*
+            "Tmax="*"$(maximal_temperature_array[1])" *", "*
             "MCSteps="*"$(nr_monte_carlo_steps_per_temperature_array[1])"*
             "}\$"),
         top_margin=5Plots.PlotMeasures.mm,
@@ -196,8 +284,8 @@ function scatter_plot_for_mulitple_gml(;
     @df df StatsPlots.scatter!(
         :BondLenghtStd, 
         :BondAngleStd, 
-        colorbar_title="GradTLog10",
-        zcolor=:GradTLog10,
+        colorbar_title="Theta",
+        zcolor=:Theta,
         color=cgrad(:roma, rev=true),
         shape=:Shape,
         subplot=3)
@@ -210,6 +298,71 @@ function scatter_plot_for_mulitple_gml(;
         color=cgrad(:roma, rev=true),
         shape=:Shape,
         subplot=4)
+
+
+
+
+
+
+
+    # plot
+
+    B=@df df Plots.scatter(
+        #plot_title=plot_title,
+        plot_title=LaTeXStrings.LaTeXString("Bond length std diff log10 vs Bond angle std diff log10 \n\$^\\textrm{"*
+            "N="*"$(nr_vertices_array[1])" *", "*
+            "Tmax="*"$(maximal_temperature_array[1])" *", "*
+            "MCSteps="*"$(nr_monte_carlo_steps_per_temperature_array[1])"*
+            "}\$"),
+        top_margin=5Plots.PlotMeasures.mm,
+        titlefont = font(6),
+        layout = (2,2),
+        size = (900, 700),
+        colorbar=:top,
+        legend=false,
+        xlabel="Bond length std diff log10 / d", 
+        ylabel="Bond angle std diff log10 / rad"
+        )
+
+    @df df StatsPlots.scatter!(
+        :BondLenghtStdDiffLog10, 
+        :BondAngleStdDiffLog10, 
+        colorbar_title="MaxT",
+        zcolor=:MaxT,
+        color=cgrad(:roma, rev=true),
+        shape=:Shape,
+        subplot=1)
+
+    @df df StatsPlots.scatter!(
+        :BondLenghtStdDiffLog10, 
+        :BondAngleStdDiffLog10,
+        colorbar_title="Beta",
+        zcolor=:Beta,
+        color=cgrad(:roma, rev=true),
+        shape=:Shape,
+        subplot=2)
+
+    @df df StatsPlots.scatter!(
+        :BondLenghtStdDiffLog10, 
+        :BondAngleStdDiffLog10,
+        colorbar_title="Theta",
+        zcolor=:Theta,
+        color=cgrad(:roma, rev=true),
+        shape=:Shape,
+        subplot=3)
+    
+    @df df StatsPlots.scatter!(
+        :BondLenghtStdDiffLog10, 
+        :BondAngleStdDiffLog10,
+        colorbar_title="AcceptedMovesLog10",
+        zcolor=:AcceptedMovesLog10,
+        color=cgrad(:roma, rev=true),
+        shape=:Shape,
+        subplot=4)    
+
+
+
+
 
     # plot path and name
 
@@ -240,9 +393,24 @@ function scatter_plot_for_mulitple_gml(;
         *"_Theta_GS="*"$minimum_theta" * "-" * "$maximum_theta"
         *"_Trials="*"$nr_trials_per_temperature"
         *".png")
+
+    plot_filename_diff = (plot_filename_start
+        *"d_"
+        *"_N="*"$minimum_nr_vertices" * "-" * "$maximum_nr_vertices"
+        *"_T="*"$minimum_temperature" * "-" * "$maximum_temperature"
+        *"_Beta="*"$minimum_bond_bending_const" * "-" * "$maximum_bond_bending_const"
+        *"_GradT="*"$minimum_temperature_gradient" * "-" * "$maximum_temperature_gradient"
+        *"_StepsPerT="*"$minimum_nr_monte_carlo_steps_per_temperature" * "-" * "$maximum_nr_monte_carlo_steps_per_temperature"
+        *"_Theta_GS="*"$minimum_theta" * "-" * "$maximum_theta"
+        *"_Trials="*"$nr_trials_per_temperature"
+        *".png")
+
     plot_total_path=plot_save_path*plot_filename
+    plot_total_path_diff=plot_save_path*plot_filename_diff
 
     Plots.savefig(A,plot_total_path)
+
+    Plots.savefig(B,plot_total_path_diff)
 
 end
 
@@ -250,13 +418,13 @@ end
 scatter_plot_for_mulitple_gml(
     nr_vertices_array=[216],
     maximal_temperature_array=[0.1,0.135,0.17,0.205,0.24],
-    bond_bending_const_array=[0,0.285,0.5,1,2],
-    temperature_gradient_array=[10,1,0.1,0.01,0.001],
+    bond_bending_const_array=[0,0.21,0.285,0.36,0.5],
+    temperature_gradient_array=[0.1],
     nr_monte_carlo_steps_per_temperature_array=[0.01],
     theta_ground_state_array=[110.0,180.0],
-    nr_trials_per_temperature=1,  
+    nr_trials_per_temperature=1, 
     save_path = raw".\simulations\multiple_parameters\\",
-    filename_start = "m_BTMC",    
+    filename_start = "m_BTMC_q_t_",    
     plot_save_path = raw".\simulations\analysis_plot\\",
-    plot_filename_start = "m_BTMC_matrix_1"
+    plot_filename_start = "m_BTMC_matrix_q_t_5_"
 )
